4233d0028109bd105ba7cbe9d0f9553b802eb695
MarcRuizenti <146344001+MarcRuizenti@users.noreply.github.com>
2024-11-22 16:37:13 +0100

    bandera

--- Assets/Scripts/CubeLogic.cs
+++ Assets/Scripts/CubeLogic.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CubeLogic : MonoBehaviour
 {
+    [SerializeField] private Material _bandera;
+    [SerializeField] private Material _default;
+    private bool bandera = false;
+    private bool posibleBandera = false;
+
     public Vector3 pos;
     public LogicMap _logicMap;
     public bool click = false;
@@ -11,7 +16,7 @@ public class CubeLogic : MonoBehaviour
 
     private void Update()
     {
-        if (click && !IAmclick) 
+        if (click && !IAmclick && !bandera) 
         {
             if (_logicMap != null)
             {
@@ -20,6 +25,7 @@ public class CubeLogic : MonoBehaviour
                 IAmclick = true;
             }
         }
+        click = false;
     }
 
     private void OnMouseOver()
@@ -28,11 +34,35 @@ public class CubeLogic : MonoBehaviour
         {
             if (_logicMap != null)
             {
-                if (!IAmclick) 
+                if (!IAmclick && !bandera) 
                 { 
                     _logicMap.Click(this.gameObject);
                     IAmclick = true;
                 }
+            }
+        }
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            posibleBandera = true;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            if (posibleBandera)
+            {
+                if (!bandera)
+                {
+                    transform.GetComponent<Renderer>().material = _bandera;
+                    bandera = !bandera;
+                }
+                else
+                {
+                    transform.GetComponent<Renderer>().material = _default;
+                    bandera = !bandera;
+                }
+
+                posibleBandera = false;
             }
         }
     }
