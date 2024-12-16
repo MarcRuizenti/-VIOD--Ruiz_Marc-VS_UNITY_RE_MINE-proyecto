feedcc158f179eebca626f9fd76a37df7d3324ff
MarcRuizenti <146344001+MarcRuizenti@users.noreply.github.com>
2024-12-16 17:53:35 +0100

    Assets Menu Options

--- Assets/Scripts/UI/Sounds/SliderManager.cs
+++ Assets/Scripts/UI/Sounds/SliderManager.cs
@@ -24,15 +24,14 @@ public class SliderManager : MonoBehaviour
 
     public void SetMusicVolume()
     {
-        audioMixer.SetFloat("music", Mathf.Log10(musicSlider.value)*20);
+        audioMixer.SetFloat("music", Mathf.Log10(musicSlider.value) * 20);
         PlayerPrefs.SetFloat("musicVolume", musicSlider.value);
-
     }
+
     public void SetSFXVolume()
     {
         audioMixer.SetFloat("SFX", Mathf.Log10(sfxSlider.value) * 20);
         PlayerPrefs.SetFloat("SFXVolume", sfxSlider.value);
-
     }
 
     private void Load()
@@ -43,6 +42,4 @@ public class SliderManager : MonoBehaviour
         SetMusicVolume();
         SetSFXVolume();
     }
-
-
 }
